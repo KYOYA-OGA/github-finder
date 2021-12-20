9b824f732f58101854d1adf23c11065d6dcfca79
KYOYA OGASAWARA <kyoyaoga@gmail.com>
2021-12-20 19:24:59 +0700

    first commit

---
+++ src/components/users/UserResults.jsx
@@ -0,0 +1,26 @@
+import { useContext, useEffect } from 'react';
+import Spinner from '../layout/Spinner';
+import UserItem from './UserItem';
+import GithubContext from '../../context/github/GithubContext';
+
+function UserResults() {
+  const { loading, users, fetchUsers } = useContext(GithubContext);
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  if (!loading) {
+    return (
+      <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        {users.map((user) => {
+          return <UserItem key={user.id} user={user} />;
+        })}
+      </div>
+    );
+  } else {
+    return <Spinner />;
+  }
+}
+
+export default UserResults;
